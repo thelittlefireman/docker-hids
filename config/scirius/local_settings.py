f417f3d338c5f50cb1dc04ef007a9c92d51735f7
Thomas Goureau <thomas.goureau@mythalesgroup.com>
2018-11-23 11:06:26 +0000

    improve network connexion and add some informations

--- config/scirius/local_settings.py
+++ config/scirius/local_settings.py
@@ -1,11 +1,13 @@
 USE_ELASTICSEARCH = True
-ELASTICSEARCH_ADDRESS = "localhost:9200"
+ELASTICSEARCH_ADDRESS = "elasticsearch:9200"
 ELASTICSEARCH_VERSION = 6 # In 1, 2, 5 set depending on ES major version
 
 USE_KIBANA = True
-KIBANA_URL = "http://localhost:5601/"
+KIBANA_URL = "http://kibana:5601/"
 
 DATA_DIR = "/sciriusdata/"
 
+SURICATA_UNIX_SOCKET = "/var/run/suricata/suricata-command.socket"
+
 USE_EVEBOX = True
-EVEBOX_ADDRESS = "localhost:5636"
+EVEBOX_ADDRESS = "evebox:5636"
--- suricata/docker-entrypoint.sh
+++ suricata/docker-entrypoint.sh
@@ -6,4 +6,4 @@ if [ "${1:0:1}" != "-" ]; then
     PS1=${PS1} exec "$@"
 fi
 
-exec suricata -c /etc/suricata/suricata.yaml -i $INTERFACE_NAME $@
+exec LD_PRELOAD="/usr/lib/libtcmalloc_minimal.so.0" suricata -c /etc/suricata/suricata.yaml -i $INTERFACE_NAME $@
